bf6c3b589c70cef39c607d799dcb0206f009b788
cc <18856836718@163.com>
2024-03-05 22:19:29 +0800

    解决本机安装问题

--- component/BiShengCompiler/check_install_result.sh
+++ component/BiShengCompiler/check_install_result.sh
@@ -4,5 +4,9 @@ clang_path=$(which clang)
 if [[ ${clang_path} == ${HOME}/.local/BiShengCompiler-3.2.0-aarch64-linux/bin/clang ]]; then
     echo "true"
 else
-    echo "false"
+    if [[ -f ${HOME}/.local/BiShengCompiler-3.2.0-aarch64-linux/bin/clang ]] && [[ $(grep -A1 '^export BISHENG_COMPILER_HOME=${HOME}/.local/BiShengCompiler-3.2.0-aarch64-linux/bin$' ${HOME}/.bashrc | grep '^export PATH=${BISHENG_COMPILER_HOME}:${PATH}$' | wc -l) == "1" ]]; then
+        echo "true"
+    else
+        echo "false"
+    fi
 fi
--- tools/install_dependency/src/handler/connect_check.py
+++ tools/install_dependency/src/handler/connect_check.py
@@ -1,9 +1,12 @@
 import logging
+import socket
+
 import constant
 from handler.handler_and_node import Handler
 from machine.scanner_machine import ScannerMachine
 from machine.builder_machine import BuilderMachine
 from machine.executor_machine import ExecutorMachine
+from machine.local_machine import LocalMachine
 from exception.connect_exception import ConnectException
 
 LOGGER = logging.getLogger("install_dependency")
@@ -18,17 +21,24 @@ class ConnectCheck(Handler):
 
     def handle(self, data) -> bool:
         LOGGER.debug("ConnectCheck start!")
+        local_ip = ConnectCheck.get_local_ip()
+
         ret = True
         for role in ConnectCheck.klass_dict:
-            ret = ret and ConnectCheck.machine_role_check(data, role)
+            ret = ret and ConnectCheck.machine_role_check(data, role, local_ip)
         return ret
-    
+
     @staticmethod
-    def machine_role_check(data, role):
+    def machine_role_check(data, role, local_ip):
         builder_list = data.get(role)
         klass = ConnectCheck.klass_dict.get(role)
         data[role + constant.MACHINE] = dict()
         for ip in builder_list:
+            if ip == local_ip or ip == "127.0.0.1":
+                ip = "127.0.0.1"
+                machine_instance = LocalMachine(ip)
+                data[role + constant.MACHINE][ip] = machine_instance
+                continue
             try:
                 machine_instance = klass(ip, data[constant.USER], data[constant.PKEY],
                                          data.get(constant.PASSWORD, None))
@@ -42,3 +52,13 @@ class ConnectCheck(Handler):
                 del data[role + constant.MACHINE]
                 return False
         return True
+
+    @staticmethod
+    def get_local_ip():
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            sock.connect(("8.8.8.8", 80))
+            ip = sock.getsockname()[0]
+        finally:
+            sock.close()
+        return ip if ip else "127.0.0.1"
