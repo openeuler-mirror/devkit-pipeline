45e49c9de1631815772010046ffac2f7bfeeda7f
Caohongtao <caohongtao_yewu@cmss.chinamobile.com>
2024-04-30 07:43:08 +0000

    解决检测lkp是否安装成功的逻辑有误的问题 由于是安装在/root/.local/lkp-tests/lkp,软连接到/usr/local/bin
    Signed-off-by: Caohongtao <caohongtao_yewu@cmss.chinamobile.com>

--- component/LkpTests/check_install_result.sh
+++ component/LkpTests/check_install_result.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 lkp_path=$(which lkp)
-if [[ "${lkp_path}" == *"/bin/lkp"* ]]; then
+if [[ "${lkp_path}" == *"/local/bin/lkp"* ]]; then
     echo "true"
 else
     echo "false, 请确认是否配everything的yum源，以及是否给安装用户配属sudo权限"
