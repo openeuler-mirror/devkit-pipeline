dcdff986f6b19d307e38dd6da6f50b8daaf75327
cc <18856836718@163.com>
2024-06-14 16:25:16 +0800

    修改DevKit Cli安装方式，不创建软链接

--- component/CompatibilityTesting/install.sh
+++ component/CompatibilityTesting/install.sh
@@ -4,6 +4,7 @@ set -e
 
 
 function main (){
+    mkdir -p ${HOME}/.local
     compatibility_test_tar=/tmp/devkitdependencies/compatibility_testing.tar.gz
     echo "Decompress compatibility_testing.tar.gz to ${HOME}/.local"
     tar --no-same-owner -zxf ${compatibility_test_tar} -C ${HOME}/.local/
