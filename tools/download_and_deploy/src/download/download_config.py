6e9e14e76b76992c30801c5dc7edf6ae69166d3b
zoupanpan <e19901004@163.com>
2024-06-14 16:40:10 +0800

    下载路径更新

--- tools/download_and_deploy/src/download/download_config.py
+++ tools/download_and_deploy/src/download/download_config.py
@@ -21,19 +21,19 @@ BiShengJDK17 = {
 
 CompatibilityTesting = {
     "component_name": "CompatibilityTesting",
-    "file": "https://gitee.com/openeuler/devkit-pipeline/releases/download/v1.0.1/compatibility_testing.tar.gz",
+    "file": "https://gitee.com/openeuler/devkit-pipeline/releases/download/v1.0.2-beta/compatibility_testing.tar.gz",
     "file_size": "76451308",
 }
 
 DevKitTester = {
     "component_name": "DevKitTester",
-    "file": "https://gitee.com/bdgl/devkit-pipeline/releases/download/compatibility_testing-v1.2/devkit_tester.tar.gz",
-    "file_size": "33426960",
+    "file": "https://gitee.com/openeuler/devkit-pipeline/releases/download/v1.0.2-beta/devkit_tester.tar.gz",
+    "file_size": "36151864",
 }
 
 A_FOT = {
     "component_name": "A-FOT",
-    "file": "https://gitee.com/openeuler/devkit-pipeline/releases/download/v0.2/a-fot.tar.gz",
+    "file": "https://gitee.com/openeuler/devkit-pipeline/releases/download/v1.0.2-beta/a-fot.tar.gz",
     "file_size": "15740",
 }
 
