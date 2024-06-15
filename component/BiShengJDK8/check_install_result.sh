f3413d2f2f31dac9a95c6a3db548650147b00362
cc <18856836718@163.com>
2024-06-15 15:15:48 +0800

    新增JDK部署时配置环境变量

--- component/BiShengJDK8/check_install_result.sh
+++ component/BiShengJDK8/check_install_result.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-if [[ -f ${HOME}/.local/bisheng-jdk1.8.0_402/bin/java ]]; then
+java_path=$(which java)
+if [[ ${java_path} == ${HOME}/.local/bisheng-jdk1.8.0_402/bin/java ]]; then
     echo "true"
 else
-    echo "false"
+    if [[ -f ${HOME}/.local/bisheng-jdk1.8.0_402/bin/java ]] && [[ $(grep -A1 '^export JAVA_HOME=${HOME}/.local/bisheng-jdk1.8.0_402$' ${HOME}/.bashrc | grep '^export PATH=${JAVA_HOME}/bin:${PATH}$' | wc -l) == "1" ]]; then
+        echo "true"
+    else
+        echo "false"
+    fi
 fi
