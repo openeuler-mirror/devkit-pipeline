bd8b88dd56ab7ba5a9766f7305dfac3224d806cd
zoupanpan <e19901004@163.com>
2024-06-18 12:57:14 +0800

    添加对docker的支持

--- common/devkit_utils/docker_utils.py
+++ common/devkit_utils/docker_utils.py
@@ -5,22 +5,22 @@ from devkit_utils import shell_tools
 
 
 def get_docker_id(docker_path: str):
-    paths_name = docker_path.split("/")
-    if len(paths_name) >= 2:
-        return paths_name[1]
+    paths_name = docker_path.strip().split("/")
+    if len(paths_name) >= 3:
+        return paths_name[2]
     else:
         raise Exception("can not found docker id")
 
 
 def is_docker_process(pid):
     cgroup_file = f'/proc/{pid}/cgroup'
-    if not os.path.exists(pid):
+    if not os.path.exists(cgroup_file):
         return False, None
     with open(cgroup_file, "r", encoding="utf-8") as file:
         cgroup_infos = file.readlines()
         for line in cgroup_infos:
-            fields = line.strip(":")
-            if len(fields) == 3 and str(fields[1]) == "devices" and str(fields[2]).startswith("/docker"):
+            fields = line.split(":")
+            if len(fields) >= 3 and str(fields[1]) == "devices" and str(fields[2]).startswith("/docker"):
                 return True, get_docker_id(fields[2])
     return False, None
 
