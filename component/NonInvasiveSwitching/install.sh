0d2c00ba2b276279d5a64fbf7b5497a7c00a907a
cc <18856836718@163.com>
2024-09-10 15:47:37 +0800

    无感切换bug-fix

--- component/NonInvasiveSwitching/install.sh
+++ component/NonInvasiveSwitching/install.sh
@@ -17,23 +17,24 @@ EOF
 set -x
 "${HOME}"/.local/BiShengCompiler-3.2.0-aarch64-linux/bin/clang++ "$@" -mcpu=tsv110 -O3 -g
 EOF
-chmod +x "${HOME}"/.local/wrap-bin/clang
-chmod +x "${HOME}"/.local/wrap-bin/clang++
-chmod +x "${HOME}"/.local/wrap-bin/gcc
-chmod +x "${HOME}"/.local/wrap-bin/g++
+    chmod +x "${HOME}"/.local/wrap-bin/clang
+    chmod +x "${HOME}"/.local/wrap-bin/clang++
+    chmod +x "${HOME}"/.local/wrap-bin/gcc
+    chmod +x "${HOME}"/.local/wrap-bin/g++
 
-  cat > "${HOME}"/.local/wrap-bin/devkit_pipeline.sh <<'EOF'
+    cat > "${HOME}"/.local/wrap-bin/devkit_NonInvasiveSwitching.sh <<'EOF'
 
-export PATH="${HOME}"/.local/BiShengCompiler-3.2.0-aarch64-linux/bin:/usr/local/bin:$PATH
-export LD_LIBRARY_PATH="${HOME}"/.local/BiShengCompiler-3.2.0-aarch64-linux/lib:$LD_LIBRARY_PATH
+export PATH=${HOME}/.local/BiShengCompiler-3.2.0-aarch64-linux/bin:${HOME}/.local/wrap-bin:$PATH
+export LD_LIBRARY_PATH=${HOME}/.local/BiShengCompiler-3.2.0-aarch64-linux/lib:$LD_LIBRARY_PATH
+
 EOF
-chmod 755 "${HOME}"/.local/wrap-bin/devkit_pipeline.sh
+    chmod 755 "${HOME}"/.local/wrap-bin/devkit_NonInvasiveSwitching.sh
 }
 
 if [ -d "${HOME}"/.local/wrap-bin ]; then
-  main
+    main
 else
-  mkdir -p "${HOME}"/.local/wrap-bin
-  main
+    mkdir -p "${HOME}"/.local/wrap-bin
+    main
 fi
 
