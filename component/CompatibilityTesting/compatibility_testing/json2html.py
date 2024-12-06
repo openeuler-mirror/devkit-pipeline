6a3860f28622b6d54898fd2806800ba61dc19156
cc <18856836718@163.com>
2024-12-06 14:08:48 +0800

    兼容性测试工具重构

--- component/CompatibilityTesting/compatibility_testing/json2html.py
+++ component/CompatibilityTesting/compatibility_testing/json2html.py
@@ -9,18 +9,19 @@ COMPATIBILITY_JSON_NAME = "log.json"
 def compatibility_result_to_html():
     compatibility_result = []
     compatibility_result.extend(COMPATIBILITY_REPORT_ENTRIES)
-    with open(os.path.join(os.getcwd(), "Chinese", COMPATIBILITY_JSON_NAME), "r") as file:
+    with open(os.path.join(os.getcwd(), COMPATIBILITY_JSON_NAME), "r") as file:
         data = json.load(file)
 
         for item in data:
             compatibility_result.extend([item.get(entry) for entry in COMPATIBILITY_REPORT_ENTRIES])
 
     compatibility_report = json.dumps(compatibility_result)
-    with open(os.path.join(os.getcwd(), "template.html"), "r") as file:
-        html_lines = file.readlines()
-        res = [sub.replace(COMPATIBILITY_REPORT_TEMPLATE_HOLDER, compatibility_report) for sub in html_lines]
-        with open(os.path.join(os.getcwd(), "compatibility_report.html"), "w") as f:
-            f.writelines(res)
-
+    with open("template.html", "r") as input_file, open("compatibility_report.html", "w") as output_file:
+        html_lines = input_file.readlines()
+        temp = html_lines[21]
+        html_lines[21] = temp.replace(COMPATIBILITY_REPORT_TEMPLATE_HOLDER, compatibility_report)
+        output_file.writelines(html_lines)
+    
+        
 
 compatibility_result_to_html()
