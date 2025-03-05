00021bd766dea90563043f5a7b5b290bb1b1cd2a
devkit-pipeline <3361635964@qq.com>
2025-03-05 02:15:38 +0000

    update component/DevKitCLI/install.sh.
    Signed-off-by: devkit-pipeline <3361635964@qq.com>

--- component/DevKitCLI/install.sh
+++ component/DevKitCLI/install.sh
@@ -4,14 +4,14 @@ cd /tmp/devkitdependencies/
 
 function main() {
     mkdir -p ${HOME}/.local
-    echo "Decompress DevKit-CLI-24.0.RC3-Linux-Kunpeng.tar.gz to ${HOME}/.local"
-    tar --no-same-owner -zxf /tmp/devkitdependencies/DevKit-CLI-24.0.RC3-Linux-Kunpeng.tar.gz -C ${HOME}/.local
-    echo "Decompress DevKit-CLI-24.0.RC3-Linux-Kunpeng.tar.gz to ${HOME}/.local finished."
+    echo "Decompress DevKit-CLI-24.0.T50-Linux-Kunpeng.tar.gz to ${HOME}/.local"
+    tar --no-same-owner -zxf /tmp/devkitdependencies/DevKit-CLI-24.0.T50-Linux-Kunpeng.tar.gz -C ${HOME}/.local
+    echo "Decompress DevKit-CLI-24.0.T50-Linux-Kunpeng.tar.gz to ${HOME}/.local finished."
 
     echo "change ${HOME}/.bashrc"
-    sed -i '/DevKit-CLI-24.0.RC3-Linux-Kunpeng/d' ${HOME}/.bashrc
+    sed -i '/DevKit-CLI-24.0.T50-Linux-Kunpeng/d' ${HOME}/.bashrc
     cat >> ${HOME}/.bashrc <<'EOF'
-export PATH=${HOME}/.local/DevKit-CLI-24.0.RC3-Linux-Kunpeng:${PATH}
+export PATH=${HOME}/.local/DevKit-CLI-24.0.T50-Linux-Kunpeng:${PATH}
 EOF
 
     echo "source ${HOME}/.bashrc"
