1676cb53d137a359bb01d73302808df5978f4b12
devkit-pipeline <3361635964@qq.com>
2025-03-05 02:16:35 +0000

    update tools/download_and_deploy/src/component_install/devkitcli_deploy.py.
    Signed-off-by: devkit-pipeline <3361635964@qq.com>

--- tools/download_and_deploy/src/component_install/devkitcli_deploy.py
+++ tools/download_and_deploy/src/component_install/devkitcli_deploy.py
@@ -6,7 +6,7 @@ from component_install.deploy_base import DeployBase
 class DevkitCLIDeploy(DeployBase):
     component_name = "DevKitCLI"
     remote_file_list = [
-        os.path.join("/tmp", constant.DEPENDENCY_DIR, "DevKit-CLI-24.0.RC3-Linux-Kunpeng.tar.gz"),
+        os.path.join("/tmp", constant.DEPENDENCY_DIR, "DevKit-CLI-24.0.T50-Linux-Kunpeng.tar.gz"),
         os.path.join("/tmp", constant.DEPENDENCY_DIR, "DevKitCLI" + "install.sh"),
         os.path.join("/tmp", constant.DEPENDENCY_DIR, "DevKitCLI" + "check_install_result.sh"),
     ]
